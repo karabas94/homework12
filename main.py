ea69aa1a06d0b628f765e3411f64cfd11f650d4a
karabas94 <valet14051994@gmail.com>
2022-10-31 23:40:16 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+Написать класс ITMan
+аттрибуты:
+    'firstname': 'Ivasik',
+    'lastname': 'Telesik',
+    'age': 13,
+    'e-mail': 'ivasik-telesik1732@izkrnanog.ua',
+    'skils': ['ходить', "говорить", "кодить"],
+    'people_lang': ['Україньська', "Англійська"],
+    'coding_lang': ['Python', "C++", "lisp"],
+методы на свое усмотрение(удобство оценивается)
+
+со звездочкой
+добавить возможность добавления объектов класса в список
+добавить инструментарий хранения этого списка в файле(JSON) между запусками программы
+"""
+
+
+class ITMan:
+    li = []
+
+    def __init__(self):
+        self.firstname = str(input("Enter Name: "))
+        self.lastname = str(input("Enter Last name: "))
+        self.age = int(input("Enter Age: "))
+        self.e_mail = str(input("Enter E-mail: "))
+        self.skills = str.split(input("Enter skills: "))
+        self.people_lang = str.split(input("Enter speaking languish: "))
+        self.coding_lang = str.split(input("Enter IT languish:"))
+
+    def __str__(self):
+        return f'ITPeople: Firstname: {self.firstname}, Lastname: {self.lastname}, Age: {self.age}, E-mail: {self.e_mail}, ' \
+               f'Skills: {self.skills}, Speaking languish: {self.people_lang}, IT languish: {self.coding_lang}'
+
+
+print(ITMan())
